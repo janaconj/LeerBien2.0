42c0f5f423640b8bdca4da1fbb728835ebb5dd70
Jana con J <janiux200994@gmail.com>
2018-07-04 03:26:41 -0400

    imagenes, sonidos y estilos

--- js/drag.js
+++ js/drag.js
@@ -1,6 +1,7 @@
 $(function(){
     
     $("#drag").draggable({start: function(){}});
+    
     $("#dragva").draggable({start: function(){
         $("#audioa")[0].play();
     }}, {helper: 'clone'});
@@ -46,6 +47,43 @@ $(function(){
     }}, {helper: 'clone'});
     $("#dragf8").draggable({start: function(){
         $("#audio8")[0].play();
+    }}, {helper: 'clone'});
+    
+    $("#dragcb").draggable({start: function(){
+        $("#audiob")[0].play();
+    }}, {helper: 'clone'});
+    $("#dragcc").draggable({start: function(){
+        $("#audioc")[0].play();
+    }}, {helper: 'clone'});
+    $("#dragcc1").draggable({start: function(){
+        $("#audioc")[0].play();
+    }}, {helper: 'clone'});
+    $("#dragcd").draggable({start: function(){
+        $("#audiod")[0].play();
+    }}, {helper: 'clone'});
+    $("#dragcf").draggable({start: function(){
+        $("#audiof")[0].play();
+    }}, {helper: 'clone'});
+    $("#dragcg").draggable({start: function(){
+        $("#audiog")[0].play();
+    }}, {helper: 'clone'});
+    $("#dragch").draggable({start: function(){
+        $("#audioh")[0].play();
+    }}, {helper: 'clone'});
+    $("#dragcm").draggable({start: function(){
+        $("#audiom")[0].play();
+    }}, {helper: 'clone'});
+    $("#dragcn").draggable({start: function(){
+        $("#audion")[0].play();
+    }}, {helper: 'clone'});
+    $("#dragcm1").draggable({start: function(){
+        $("#audiom")[0].play();
+    }}, {helper: 'clone'});
+    $("#dragcn1").draggable({start: function(){
+        $("#audion")[0].play();
+    }}, {helper: 'clone'});
+    $("#dragcl").draggable({start: function(){
+        $("#audiol")[0].play();
     }}, {helper: 'clone'});
     
     $("#drop").droppable({
@@ -170,7 +208,129 @@ $(function(){
         }
     });
     
+    $("#dropc1").droppable({
+        drop: function(event, ui){
+            var id = $(ui.draggable).attr("id");
+            if ($("#dropc1").is(':empty')){
+                if (id == 'dragf4' || id == 'dragf6' || id == 'dragf8'){
+                    switch(id){
+                    case 'dragf4':
+                        $("#dropc1").html("<img class='foto' src='images/camion.jpg'>");
+                        $("#dragf4").hide();
+                        break;
+                    case 'dragf6':
+                        $("#dropc1").html("<img class='foto' src='images/canoa.jpg'>");
+                        $("#dragf6").hide();
+                        break; 
+                    case 'dragf8':
+                        $("#dropc1").html("<img class='foto' src='images/coche.jpg'>");
+                        $("#dragf8").hide();
+                        break; 
+                    }
+                }
+            }  
+        }
+    });
+    
+    $("#dropc2").droppable({
+        drop: function(event, ui){
+            var id = $(ui.draggable).attr("id");
+            if ($("#dropc2").is(':empty')){
+                if (id == 'dragf4' || id == 'dragf6' || id == 'dragf8'){
+                    switch(id){
+                    case 'dragf4':
+                        $("#dropc2").html("<img class='foto' src='images/camion.jpg'>");
+                        $("#dragf4").hide();
+                        break;
+                    case 'dragf6':
+                        $("#dropc2").html("<img class='foto' src='images/canoa.jpg'>");
+                        $("#dragf6").hide();
+                        break; 
+                    case 'dragf8':
+                        $("#dropc2").html("<img class='foto' src='images/coche.jpg'>");
+                        $("#dragf8").hide();
+                        break; 
+                    }
+                }
+            }  
+        }
+    });
+    
+    $("#dropc3").droppable({
+        drop: function(event, ui){
+            var id = $(ui.draggable).attr("id");
+            if ($("#dropc3").is(':empty')){
+                if (id == 'dragf4' || id == 'dragf6' || id == 'dragf8'){
+                    switch(id){
+                    case 'dragf4':
+                        $("#dropc3").html("<img class='foto' src='images/camion.jpg'>");
+                        $("#dragf4").hide();
+                        break;
+                    case 'dragf6':
+                        $("#dropc3").html("<img class='foto' src='images/canoa.jpg'>");
+                        $("#dragf6").hide();
+                        break; 
+                    case 'dragf8':
+                        $("#dropc3").html("<img class='foto' src='images/coche.jpg'>");
+                        $("#dragf8").hide();
+                        break; 
+                    }
+                }
+            }  
+        }
+    });
+    
+    $("#dropcc").droppable({
+        drop: function(event, ui){
+            var id = $(ui.draggable).attr("id");
+            if (id == 'dragcc' ){
+                /*$("#audioe")[0].play();*/
+                $("#dropcc").html("<div class='peloto c'>C</div>");
+                $("#dragcc").hide();
+                
+                if ( $("#dropcm").is(':empty') == false && $("#dropcn").is(':empty') == false){
+                    $("#audiocom")[0].play();
+                }
+            }
+        }
+    });
+    
+    $("#dropcm").droppable({
+        drop: function(event, ui){
+            var id = $(ui.draggable).attr("id");
+            if (id == 'dragcm' ){
+                /*$("#audioe")[0].play();*/
+                $("#dropcm").html("<div class='peloto o'>M</div>");
+                $("#dragcm").hide();
+                
+                if ( $("#dropcc").is(':empty') == false && $("#dropcn").is(':empty') == false){
+                    $("#audiocom")[0].play();
+                }
+            }
+        }
+    });
+    
+    $("#dropcn").droppable({
+        drop: function(event, ui){
+            var id = $(ui.draggable).attr("id");
+            if (id == 'dragcn' ){
+                /*$("#audioe")[0].play();*/
+                $("#dropcn").html("<div class='peloto n'>N</div>");
+                $("#dragcn").hide();
+                
+                if ( $("#dropcc").is(':empty') == false && $("#dropcm").is(':empty') == false){
+                    $("#audiocom")[0].play();
+                }
+            }
+        }
+    });
+    
+    
     $("#abeja").click(function() {
+        $("#audiocom")[0].play();
+    });
+    
+    $("#camion").click(function() {
         $("#audiocom")[0].play();
     });
 });
